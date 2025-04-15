2a7d43bc0bc06d75e44c844c7157db49deafd6d7
Nabievabduvohid <abduvohidnabiev99@gmail.com>
2025-04-15 22:46:33 +0500

    start

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("salom JAVASCRIPT");
+
+
+
+const ism = prompt(" ismingizi kiriting");
+console.log("Abduvohid");
+const familya = prompt ("Familiyangizni kiriting")
+console.log("Nabiev");
+const yosh = prompt("Yoshingizi kiriting")
+console.log(14);
+
